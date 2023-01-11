839a34c91af72d14c4fb2dcbf42eccd30ce25f16
joaoaguia <joaopedroaguiadasilva@gmail.com>
2023-01-11 00:49:04 +0000

    Python/Bots/Web Scraping/Public_Job_Searcher/ Updated Version of main.py

--- Python/Bots/Web Scraping/Public_Job_Searcher/main.py
+++ Python/Bots/Web Scraping/Public_Job_Searcher/main.py
@@ -10,6 +10,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import pandas as pd
+import smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from openpyxl import Workbook
 
 if getattr(sys, 'frozen', False):
@@ -18,6 +23,7 @@ elif __file__:
     application_path = os.path.dirname(__file__)
 
 # Counter just to know in witch page we are currently when the project is running
+rn_counter = 0
 counter = 0
 # Define the path for the excel file to be saved, in this case is the path of the project \Excel_Files
 directory = r'%s' % application_path+'\Excel_Files'
@@ -47,7 +53,11 @@ try:
 except TimeoutException:
     print("Loading took too much time!")
     # Creation of the funtion that will assign every value to column defined on the table for excel
+
+
+############################################################
 def extr_data():
+    rn_counter = 0
     # time.sleep(1)
 
     # here we have a away to find what element we want by entering in each other tag_name so we know that TR is inside TBODY and TBODY is inside MAIN
@@ -129,30 +139,97 @@ def extr_data():
                     'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblRequisitosNacionalidade').text
             except NoSuchElementException:
                 rn = 'Não Indicado'
+
+        if rn != 'Sim':
+            rn_counter = rn_counter + 1
+            
         table['Requisitos de Nacionalidade'].append(rn)
+
+        
 
         # time.sleep(1)
         driver.back()
-
+    return (rn_counter)
+############################################################
 
 try:
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, ">")))
     while driver.find_element(by=By.LINK_TEXT, value='>').text == '>':
         counter = counter + 1
         print(counter)
-        extr_data()
+        #extr_data()
+        rn_counter = extr_data() + rn_counter
 
         driver.find_element(by=By.LINK_TEXT, value='>').click()
 
 except NoSuchElementException:
     counter = counter + 1
     print(counter)
-    extr_data()
+    #extr_data()
+    rn_counter = extr_data() + rn_counter
+
 
     tabela_completa = pd.DataFrame(table)
     tabela_completa.to_excel(os.path.join(directory, file_name))
 
-print('File saved in', os.path.join(directory, file_name))
+############################# Envio Email #########################################
+
+subject = ("Extração Concursos Publicos" + date.today().strftime(" %d-%m-%y"))
+body = ('''Boa tarde,
+
+Segue em anexo os concursos públicos abertos para o dia''' + date.today().strftime(" %d-%m-%y") + '''
+
+Existem ''' + str(rn_counter) +''' vagas que não requerem nacionalidade Portuguesa.
+
+Obrigado
+O melhor BOT do Mundo'''
+
+)
+sender_email = "python.email.bot.0001@gmail.com"
+receiver_email = "kama.florencio@gmail.com"
+bcc_email = "joaopedroaguiadasilva@gmail.com"
+password = "xosmvvduoykabqtl"
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = bcc_email  # Recommended for mass emails
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+filename = os.path.join(directory, file_name)
+
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+# Log in to server using secure context and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, [receiver_email, bcc_email], text)
+
+######################## Fim Envio Email ############################################
+#print('File saved in', os.path.join(directory, file_name))
 driver.close()
-sys.exit()
+driver.quit()
+
 
---
+++ Python/Bots/Web Scraping/Public_Job_Searcher/run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /D C:\Users\joao_\Desktop\CONCURSO_SEARCHER
+py main.py
+
