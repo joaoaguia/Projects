ac386abb483cd7a795260a71c0912d9c4508bdf0
joaoaguia <joaopedroaguiadasilva@gmail.com>
2023-01-17 17:51:47 +0000

    Python / Public_Job_Searcher / Creation of new functions and improvement of the code

--- Python/Bots/Web Scraping/Public_Job_Searcher/Email_Sender.py
+++ Python/Bots/Web Scraping/Public_Job_Searcher/Email_Sender.py
@@ -1,69 +1,64 @@
+#********************************************************
+#Program: Email_Sender
+#Description: This scrypt is resposible to send e-mails using a Gmail as a sender email, in order to run the scrypt, we need to provide the following parameters:
+# to, bcc, subject, text, attachment, sender_data - location of the txt file ('C:/xxxxxx/xxxx/xxxxxx.txt') that have the gmail account and password, the txt need to be configured as the following example:
+#    sender_email = 'xxxxxx@gmail.com'
+#    password = 'xxxxxxxxxxxxxxxx'
+#********************************************************
+
 #Libraries
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import smtplib, ssl
+import smtplib
+from pathlib import Path
 import os
-from datetime import date
 
-def send_email_with_attach(rn_counter, directory, file_name):
-
-    subject = ("Extração Concursos Publicos" + date.today().strftime(" %d-%m-%y"))
-    body = ('''Boa tarde,
-
-    Segue em anexo os concursos públicos abertos para o dia''' + date.today().strftime(" %d-%m-%y") + '''
-
-    Existem ''' + str(rn_counter) +''' vagas que não requerem nacionalidade Portuguesa.
-
-    Obrigado
-    O melhor BOT do Mundo'''
-
-    )
-    
-    #Open txt where sender_email and password of that email is stored
+def send_email(to, bcc, subject, text, attachment, sender_data):
+    #Open txt where the sender email and password is stored
     expected_vars = {'sender_email':None, 'password':None}
-    with open('D:/Particulares/Joao/Estudos/Programacao/GIT/Email_data.txt', 'r') as file:  
+    with open(sender_data, 'r') as file:  
         exec(file.read(), expected_vars)
-
-    receiver_email = "kama.florencio@gmail.com"
-    bcc_email = "joaopedroaguiadasilva@gmail.com"
-
+    
     # Create a multipart message and set headers
     message = MIMEMultipart()
     message["From"] = expected_vars['sender_email']
-    message["To"] = receiver_email
+    message["To"] = to
     message["Subject"] = subject
-    message["Bcc"] = bcc_email  # Recommended for mass emails
-
+    message["Bcc"] = bcc  # Recommended for mass emails
     # Add body to email
-    message.attach(MIMEText(body, "plain"))
-    filename = os.path.join(directory, file_name)
+    message.attach(MIMEText(text, "plain"))
 
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-        # Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
+    if Path(attachment).is_file():
+        # Open attachment file in binary mode
+        with open(attachment, "rb") as attach:
+            # Add file as application/octet-stream
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attach.read())
+        # Encode file in ASCII characters to send by email    
+        encoders.encode_base64(part)
+        # Add header as key/value pair to attachment part
+        filename = os.path.basename(attachment)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename={filename}",
+        )
+        # Add attachment to message and convert message to string
+        message.attach(part)
+    else:
+        print(f"{attachment} not found.")
+        return
 
-    # Encode file in ASCII characters to send by email    
-    encoders.encode_base64(part)
-
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
-
-    # Add attachment to message and convert message to string
-    message.attach(part)
     text = message.as_string()
 
-    # Log in to server using secure context and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    try:
+        # Log in to server using secure context and send email
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         server.login(expected_vars['sender_email'], expected_vars['password'])
-        server.sendmail(expected_vars['sender_email'], [receiver_email, bcc_email], text)
-
- 
+        server.sendmail(expected_vars['sender_email'], [to, bcc], text)
+        print(f'Email sent to {to}')
+    except smtplib.SMTPException as e:
+        print(f'An error occurred: {e}')
+    finally:
+        server.quit()
--- Python/Bots/Web Scraping/Public_Job_Searcher/main.py
+++ Python/Bots/Web Scraping/Public_Job_Searcher/main.py
@@ -12,6 +12,9 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import pandas as pd
 from openpyxl import Workbook
 import Email_Sender
+import Chromedriver
+
+driver = Chromedriver.get_chromedriver()
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -32,34 +35,25 @@ table = {'Código': [], 'Tipo Oferta': [], 'Vínculo': [], 'Carreira': [], 'Cate
          'Organismo': [], 'Habilitações Literárias': [], 'Descrição da Habilitação Literária': [], 'Data Limite': [],
          'Remuneração': [], 'Suplemento Mensal': [], 'Requisitos de Nacionalidade': []}
 
-# Definion of the path for chromedriver and defines the driver (base for all searchs)
-path = application_path + '\chromedriver'
-driver = webdriver.Chrome(path)
-
 # Website we're gonna analise and the delay of max time the program will wait to the website to open and see if the "Pesquisar" button is available
 driver.get("https://www.bep.gov.pt/pages/oferta/Oferta_Pesquisa_basica.aspx")
 delay = 3  # seconds
 
 # The program will wait for the button to be available, after hes availailable the program will click it, if the page takes to long to load, the program go for "except"
 try:
-    WebDriverWait(driver, delay).until(EC.presence_of_element_located(
-        (By.NAME, 'ctl00$ctl00$FormMasterContentPlaceHolder$ContentPlaceHolder1$ucSearch')))
-    driver.find_element(by=By.NAME,
-                        value="ctl00$ctl00$FormMasterContentPlaceHolder$ContentPlaceHolder1$ucSearch").click()
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'ctl00$ctl00$FormMasterContentPlaceHolder$ContentPlaceHolder1$ucSearch')))
+    driver.find_element(by=By.NAME,value="ctl00$ctl00$FormMasterContentPlaceHolder$ContentPlaceHolder1$ucSearch").click()
 except TimeoutException:
     print("Loading took too much time!")
     # Creation of the funtion that will assign every value to column defined on the table for excel
 
-
-############################################################
 def extr_data():
     rn_counter = 0
     # time.sleep(1)
 
     # here we have a away to find what element we want by entering in each other tag_name so we know that TR is inside TBODY and TBODY is inside MAIN
     # we could have done this with "find_element_by_xpath" and have only one variable defined, but will leave it this way in case I need  to use this in other projects
-    main = driver.find_element(by=By.ID,
-                               value="ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_GvOfertaGestao")
+    main = driver.find_element(by=By.ID,value="ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_GvOfertaGestao")
     body = main.find_element(by=By.TAG_NAME, value='tbody')
     tr = body.find_elements(by=By.TAG_NAME, value='tr')
     var = 0
@@ -68,92 +62,63 @@ def extr_data():
         var = var + 1
         variable = str(var)
 
-        link = driver.find_element(by=By.LINK_TEXT, value=(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[1]').text))
+        link = driver.find_element(by=By.LINK_TEXT, value=(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[1]').text))
         table['Código'].append(link.text)
-
-        table['Tipo Oferta'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[2]').text)
-        table['Vínculo'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[3]').text)
-        table['Carreira'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[4]').text)
-        table['Categoria'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[5]').text)
-        table['Distrito'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[6]').text)
-        table['Organismo'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[7]').text)
-        table['Habilitações Literárias'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[8]').text)
-        table['Data Limite'].append(driver.find_element_by_xpath(
-            f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[9]').text)
+        table['Tipo Oferta'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[2]').text)
+        table['Vínculo'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[3]').text)
+        table['Carreira'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[4]').text)
+        table['Categoria'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[5]').text)
+        table['Distrito'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[6]').text)
+        table['Organismo'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[7]').text)
+        table['Habilitações Literárias'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[8]').text)
+        table['Data Limite'].append(driver.find_element_by_xpath(f'/html/body/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr[{variable}]/td[9]').text)
 
         #code of the offer
         print(link.text)
         link.click()
 
         try:
-            rem = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNORemuneracao').text
+            rem = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNORemuneracao').text
         except NoSuchElementException:
-            rem = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblRemuneracao').text
+            rem = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblRemuneracao').text
         table['Remuneração'].append(rem)
-
         try:
-            sm = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNOSupMensal').text
+            sm = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNOSupMensal').text
         except NoSuchElementException:
-            sm = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblSuplemento').text
+            sm = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblSuplemento').text
         table['Suplemento Mensal'].append(sm)
-
         try:
             tab = driver.find_element(by=By.LINK_TEXT, value="Requisitos de Admissão")
             tab.click()
         except NoSuchElementException:
             pass
-
         try:
-            dhl = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblDesHabLit').text
+            dhl = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblDesHabLit').text
         except NoSuchElementException:
             try:
-                dhl = driver.find_element_by_id(
-                    'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblDescricaoHabilitacao').text
+                dhl = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblDescricaoHabilitacao').text
             except NoSuchElementException:
                 dhl = 'Não Indicado'
         table['Descrição da Habilitação Literária'].append(dhl)
-
         try:
-            rn = driver.find_element_by_id(
-                'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNOReqNac').text
+            rn = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblNOReqNac').text
         except NoSuchElementException:
             try:
-                rn = driver.find_element_by_id(
-                    'ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblRequisitosNacionalidade').text
+                rn = driver.find_element_by_id('ctl00_ctl00_FormMasterContentPlaceHolder_ContentPlaceHolder1_lblRequisitosNacionalidade').text
             except NoSuchElementException:
                 rn = 'Não Indicado'
-
+        table['Requisitos de Nacionalidade'].append(rn)
         if rn != 'Sim':
             rn_counter = rn_counter + 1
-            
-        table['Requisitos de Nacionalidade'].append(rn)
 
-        
-
-        # time.sleep(1)
         driver.back()
     return (rn_counter)
-############################################################
 
 try:
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, ">")))
     while driver.find_element(by=By.LINK_TEXT, value='>').text == '>':
         counter = counter + 1
         print(counter)
-        #extr_data()
         rn_counter = extr_data() + rn_counter
 
         driver.find_element(by=By.LINK_TEXT, value='>').click()
@@ -161,16 +126,30 @@ try:
 except NoSuchElementException:
     counter = counter + 1
     print(counter)
-    #extr_data()
     rn_counter = extr_data() + rn_counter
 
-
+#save data on excel
     tabela_completa = pd.DataFrame(table)
     tabela_completa.to_excel(os.path.join(directory, file_name))
 
-Email_Sender.send_email_with_attach(rn_counter, directory, file_name)
+email_to = "kama.florencio@gmail.com"
+email_bcc = "joaopedroaguiadasilva@gmail.com"
+email_subject = ("Extração Concursos Publicos" + date.today().strftime(" %d-%m-%y"))
+email_text = ('''Boa tarde,
 
-#print('File saved in', os.path.join(directory, file_name))
+    Segue em anexo os concursos públicos abertos para o dia''' + date.today().strftime(" %d-%m-%y") + '''
+
+    Existem ''' + str(rn_counter) +''' vagas que não requerem nacionalidade Portuguesa.
+
+    Obrigado
+    O melhor BOT do Mundo'''
+
+    )
+email_attachment = os.path.join(directory, file_name)
+email_data = 'D:/Particulares/Joao/Estudos/Programacao/GIT/Email_data.txt'
+
+Email_Sender.send_email(email_to, email_bcc, email_subject, email_text, email_attachment, email_data)
+
 driver.close()
 driver.quit()
 
