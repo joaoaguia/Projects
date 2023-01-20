46f6cf9c63bc75109a076607f9b70d090d6a8e1e
joaoaguia <joaopedroaguiadasilva@gmail.com>
2023-01-20 12:56:12 +0000

    Changed Strutures on Main branch

--- Python/Bots/Web Scraping/Public_Job_Searcher/main.py
+++ Python/Bots/Web Scraping/Public_Job_Searcher/main.py
@@ -3,28 +3,28 @@ import sys
 import os
 from datetime import date
 import pandas as pd
-from openpyxl import Workbook
-
+# Path to personal functions
+sys.path.append('D:/Particulares/Joao/Estudos/Programacao/GIT/Projects/Python/Functions')
 import Email_Sender
 import Chromedriver
 import Logs
 import Web_Scrapper
 
-if getattr(sys, 'frozen', False):
-    application_path = os.path.dirname(sys.executable)
-elif __file__:
-    application_path = os.path.dirname(__file__)
-
-directory = r'%s' % application_path+'\Excel_Files'
-
-file_name = ('Extraction_' + date.today().strftime("%d_%m_%y") + '.xlsx')
-
+# Name of the project will be used to fed the creation of the excel_files and logs folder
+project_name = 'Public_Job_Searcher'
+# Website used on the 'Web_Scrapper.scrap_data' that we're gonna web scrap
+website = "https://www.bep.gov.pt/pages/oferta/Oferta_Pesquisa_basica.aspx"
+# Table with the structure we want on our excel file
 table = {'Código': [], 'Tipo Oferta': [], 'Vínculo': [], 'Carreira': [], 'Categoria': [], 'Distrito': [],
          'Organismo': [], 'Habilitações Literárias': [], 'Descrição da Habilitação Literária': [], 'Data Limite': [],
          'Remuneração': [], 'Suplemento Mensal': [], 'Requisitos de Nacionalidade': []}
-
-website = "https://www.bep.gov.pt/pages/oferta/Oferta_Pesquisa_basica.aspx"
-
+# Name of the Excel file
+file_name = ('Extraction_' + date.today().strftime("%d_%m_%y") + '.xlsx')
+# Create the excel files directory if it does not exist
+directory = os.path.join("D:/Particulares/Joao/Estudos/Programacao/GIT/Excel_Files", project_name)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+# Email Info except email text, needs to be filled after 'Web_Scrapper.scrap_data'
 email_to = "kama.florencio@gmail.com"
 email_bcc = "joaopedroaguiadasilva@gmail.com"
 email_subject = ("Extração Concursos Publicos" + date.today().strftime(" %d-%m-%y"))
@@ -32,7 +32,7 @@ email_attachment = os.path.join(directory, file_name)
 email_data = 'D:/Particulares/Joao/Estudos/Programacao/GIT/Email_data.txt'
 
 #Logger Initializer
-logger = Logs.setup_logger(project_name='Public_Job_Searcher')
+logger = Logs.setup_logger(project_name)
 logger.info("Starting the main program.")
 
 logger.info("Starting Chromedriver.")
@@ -46,6 +46,7 @@ tabela_completa = pd.DataFrame(table)
 tabela_completa.to_excel(os.path.join(directory, file_name))
 
 logger.info("Initializing Email_Sender the main program.")
+# Email message
 email_text = ('''Boa tarde,
 
     Segue em anexo os concursos públicos abertos para o dia''' + date.today().strftime(" %d-%m-%y") + '''
@@ -54,8 +55,8 @@ email_text = ('''Boa tarde,
 
     Obrigado
     O melhor BOT do Mundo''')
-logger.info(Email_Sender.send_email(email_to, email_bcc, email_subject, email_text, email_attachment, email_data))
 
+logger.info(Email_Sender.send_email(email_to, email_bcc, email_subject, email_text, email_attachment, email_data))
 logger.info("Finishing the main program.")
 driver.close()
 driver.quit()
--- Python/Bots/Web Scraping/Public_Job_Searcher/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /D C:\Users\joao_\Desktop\CONCURSO_SEARCHER
-py main.py
-
