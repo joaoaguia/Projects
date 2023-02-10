36b145de83bc2f7aa82320d2532b1882c2db97c8
joaoaguia <joaopedroaguiadasilva@gmail.com>
2023-02-10 17:20:05 +0000

    Python / Financas_House_Auction / Added new data on the email.

--- Python/Bots/Web Scraping/Financas_House_Auction/main.py
+++ Python/Bots/Web Scraping/Financas_House_Auction/main.py
@@ -1,7 +1,7 @@
 # Libraries
 import sys
 import os
-from datetime import date
+from datetime import date, timedelta
 import pandas as pd
 # Path to personal functions
 sys.path.append('D:/Particulares/Joao/Estudos/Programacao/GIT/Projects/Python/Functions')
@@ -16,6 +16,8 @@ project_name = 'Financas_House_Auction'
 website = "https://vendas.portaldasfinancas.gov.pt/bens/consultaVendasCurso.action?tipoBem=02&tipoConsulta=*&modalidade=04&distrito=&concelho=&minimo=++.+++.+++.+++%2C++&maximo=++.+++.+++.+++%2C++&dataMin=&dataMax="
 # Table with the structure we want on our excel file
 table = {'Nº Venda': [], 'Preço Base de Venda': [], 'Data Limite para Propostas': [], 'Serviço de Finanças': [], 'Estado Actual': [], 'Modalidade': [], 'Link': []}
+# Table that will go on the Email
+table_email = {'Link': []}
 # Name of the Excel file
 file_name = ('Extraction_' + date.today().strftime("%d_%m_%y") + '.xlsx')
 # Create the excel files directory if it does not exist
@@ -37,7 +39,7 @@ logger.info("Starting Chromedriver.")
 driver = Chromedriver.get_chromedriver()
 
 logger.info("Initializing Web_Scrapper.")
-Web_Scrapper.scrap_data(driver, website, table)
+ative_last_7days=(Web_Scrapper.scrap_data(driver, website, table, table_email))
 
 logger.info("Saving Excel file.")
 tabela_completa = pd.DataFrame(table)
@@ -47,10 +49,14 @@ logger.info("Initializing Email_Sender the main program.")
 # Email message
 email_text = ('''Boa tarde,
 
-    Segue em anexo os Leilões de casas / terrenos disponiveis no site das Finanças para o dia''' + date.today().strftime(" %d-%m-%y") + '''
+Segue em anexo os Leilões de casas / terrenos disponiveis no site das Finanças para o dia''' + date.today().strftime(" %d-%m-%y") + '''
+Existem ''' + str(ative_last_7days) +''' Anuncios ativos que vão expirar até dia''' + (date.today() + timedelta(days=7)).strftime(" %d-%m-%y") +'''
 
-    Obrigado
-    O melhor BOT do Mundo''')
+Os links para os anúncios são:
+''' + "\n".join(table_email['Link']) + '''
+
+Obrigado
+O melhor BOT do Mundo''')
 
 logger.info(Email_Sender.send_email(email_to, email_bcc, email_subject, email_text, email_attachment, email_data))
 logger.info("Finishing the main program.")
